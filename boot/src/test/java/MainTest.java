e085688fd4c22628442434a043a563a713b95a30
Volodymyr Soshyn <v.soshyn@gmail.com>
2018-09-29 00:11:03 +0300

    create infrastructure

---
+++ boot/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    Main main = new Main();
+
+
+    @Test
+    void hello() {
+        assertEquals("Hello", main.hello());
+    }
+}
