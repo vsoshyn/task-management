d4647d4c1772a41553f76e51409e773bc804cf37
Volodymyr Soshyn <v.soshyn@gmail.com>
2018-09-29 18:41:23 +0300

    TM-2 Create liquibase sketeton

--- environment/ci/postgres/postgres-init.sql
+++ environment/ci/postgres/postgres-init.sql
@@ -1,3 +1,6 @@
-CREATE USER teamcity ENCRYPTED PASSWORD '12345678';
-CREATE DATABASE teamcity;
-GRANT ALL PRIVILEGES ON DATABASE teamcity TO teamcity;
+create user teamcity encrypted password '12345678';
+create database teamcity;
+grant all privileges on database teamcity to teamcity;
+
+create user task_manager encrypted password '12345678';
+grant all privileges on schema public to task_manager;
