a82ab9096078b67a2787231321be9e5efd70a812
Vladimir Soshyn <vsoshyn@intellias.com>
2019-02-27 09:22:05 +0100

    cleaned up

--- release/deploy.sh
+++ release/deploy.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
-cd ..
-
-TAG=$1
-
-git reset --hard HEAD
-git fetch -t
-git checkout ${TAG}
-
 docker-compose -f environment/prod/docker-compose.yml down
 docker-compose -f environment/prod/docker-compose.yml up -d postgres
 gradle update -PrunList=prod
