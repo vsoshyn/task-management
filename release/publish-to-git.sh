aaaea4ba2eea63b4cf5b231a33f7aba8f0212236
Vladimir Soshyn <vsoshyn@intellias.com>
2019-02-27 09:00:10 +0100

    tuned git push script

--- release/publish-to-git.sh
+++ release/publish-to-git.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
-git push --all git@github.com:vsoshyn/task-management.git
-git push --tags git@github.com:vsoshyn/task-management.git
-
 git checkout master
 git merge develop
-git push git@github.com:vsoshyn/task-management.git
+
+git push --all git@github.com:vsoshyn/task-management.git
+git push --tags git@github.com:vsoshyn/task-management.git
